0f06b574760a604fa4568f111bbe1071c355651f
Bhargav Karlapudi <bhargav2691@gmail.com>
2022-10-22 19:19:31 +0530

    Create script.js

---
+++ Javascript/script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+          document.getElementById("demo1").innerHTML = "This is Demo Text One";
+          document.getElementById("demo2").innerHTML = "This is Demo Text Two";
+          document.getElementById("demo3").innerHTML =
+            "This is Demo Text Three";
+        }
